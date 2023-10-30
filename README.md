b09427473bf08fa6848250426d86767ecf8f9b87
Trévis Morvany <trevismorvany@gmail.com>
2023-10-31 00:30:45 +0100

    Added the plotting code for question 1 - Created a folder for each question - Added a python script to generate the plots for the first question

---
+++ README.md
@@ -0,0 +1,21 @@
+# EDP TP2
+
+## Question 1
+
+Figures generated with the following command:
+
+```bash
+cd question_1
+python3 graph.py -s sequential.json -r random.json -o question_1.png
+```
+
+![](./question_1/separated_question_1.png)
+
+![](./question_1/merged_question_1.png)
+
+## Question 2
+
+## Question 3
+
+## Question 4
+
---
+++ question_1/graph.py
@@ -0,0 +1,88 @@
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+
+
+def get_rw_bw(data):
+    read_bw = []
+    write_bw = []
+    
+    for job in data["jobs"]:
+        read_bw.append(job["read"]["bw"])
+        write_bw.append(job["write"]["bw"])
+
+    return read_bw, write_bw
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--sequential", type=argparse.FileType("r"), help="The file containing the sequential output")
+parser.add_argument("-r", "--random", type=argparse.FileType("r"), help="The file containing the random output")
+parser.add_argument("-o", "--output", type=str, help="Output file suffix")
+args = parser.parse_args()
+
+seq_data = json.load(args.sequential)
+args.sequential.close()
+
+rand_data = json.load(args.random)
+args.random.close()
+
+
+seq_read_bw, seq_write_bw = get_rw_bw(seq_data)
+rand_read_bw, rand_write_bw = get_rw_bw(rand_data)
+
+# First plot. Two figures, one for sequential the other for random
+fig, (ax_read, ax_write) = plt.subplots(1, 2, figsize=(14, 7))
+
+x = np.arange(0, 101, 12.5)
+ax_read.plot(x, seq_read_bw, marker="o", label="sequential")
+ax_read.plot(x, rand_read_bw, marker="o", label="random")
+
+ax_read.set_title("Bandwidth for I/O operations with 100 to 0% of reads")
+ax_read.set_xlabel("Percentage of reads")
+ax_read.set_ylabel("Bandwidth (KiB/s)")
+ax_read.set_xticks(x)
+ax_read.set_xticklabels([f"{100 - p}%" for p in x])
+ax_read.legend()
+#ax.set_yscale("log")
+
+ax_write.plot(x, seq_write_bw, marker="o", label="sequential")
+ax_write.plot(x, rand_write_bw, marker="o", label="random")
+
+ax_write.set_title("Bandwidth for I/O operations with 0 to 100% of writes")
+ax_write.set_xlabel("Percentage of writes")
+ax_write.set_ylabel("Bandwidth (KiB/s)")
+ax_write.set_xticks(x)
+ax_write.set_xticklabels([f"{p}%" for p in x])
+ax_write.legend()
+
+if args.output is not None:
+    plt.savefig("separated_" + args.output, dpi=300)
+else:
+    plt.show()
+
+
+# Second plot, all lines on the same plot + log scale
+fig, ax = plt.subplots(1, figsize=(14, 7), layout="constrained")
+
+x = np.arange(0, 101, 12.5)
+
+a=ax.plot(x, seq_read_bw, marker="o", label="sequential read")#, color="#1f77b4")
+b=ax.plot(x, rand_read_bw, marker="o", label="random read")#, color="#ff7f0e")
+
+c=ax.plot(x, seq_write_bw, marker="o", label="sequential write")#, color="#1f77b4", linestyle="dashed")
+d=ax.plot(x, rand_write_bw, marker="o", label="random write")#, color="#ff7f0e", linestyle="dashed")
+
+ax.set_title("Bandwidth for I/O operations with 100 to 0% of reads")
+ax.set_xlabel("Percentage of reads")
+ax.set_ylabel("Bandwidth (KiB/s)")
+ax.set_xticks(x)
+ax.set_xticklabels([f"{p}%" for p in x])
+ax.set_yscale("symlog")
+ax.set_ylim(0, 1.1 * ax.get_ylim()[1])
+ax.legend(loc="lower center")
+
+if args.output is not None:
+    plt.savefig("merged_" + args.output, dpi=300)
+else:
+    plt.show()
